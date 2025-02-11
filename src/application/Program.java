f784c43c5ea1c82485689b1be2e298d9cb2111a5
nogsposito <viniciussposito3103@gmail.com>
2025-02-11 01:14:05 -0300

    novo commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Program {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+        
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         
         System.out.println(seller);
     }
