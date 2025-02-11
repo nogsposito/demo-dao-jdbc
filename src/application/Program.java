b6a833b53e68d6c1248c19923d9c623e42f9df9a
nogsposito <viniciussposito3103@gmail.com>
2025-02-11 02:21:55 -0300

    novo commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,6 +31,11 @@ public class Program {
         for (Seller obj : list2){
             System.out.println(obj);
         }
+
+        System.out.println("TESTE: seller insert");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id: " + newSeller.getId());
     }
     
 }
