6cfcbd5f56b3ede3c8823f8f0054157eb7df8e9d
nogsposito <viniciussposito3103@gmail.com>
2025-02-11 01:45:37 -0300

    novo commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,9 +14,10 @@ public class Program {
         
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("TESTE: seller findById");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+        
     }
     
 }
